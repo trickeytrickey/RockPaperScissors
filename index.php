4f8560ea98b1ac5f9d1d659b8fdbe98108571fd2
Caytlyn Trickey <trickeyc@umich.edu>
2016-02-11 14:34:56 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Caytlyn Trickey -Rock Paper Scissors</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Rock Paper Scissors</h1>
+<p><strong>Note:</strong> This sample code is only
+partially done and serves ony as a starting point for the assignment.
+</p>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+<p>
+
+</p>
+</div>
+</body>
